16c90877ad5b9aab89c692a449cbff6c64dc06a1
Espen A. Kristiansen <post@espenak.net>
2012-05-09 00:59:52 +0200

    Adde more to __init__.

--- restfulgrok/__init__.py
+++ restfulgrok/__init__.py
@@ -3,4 +3,8 @@ from mock import MockResponse
 from mock import MockContext
 from mock import MockRestView
 from view import GrokRestViewMixin
+from contenttype import ContentTypesRegistry
+from contenttype import JsonContentType
+from contenttype import YamlContentType
 from fancyhtmlview import GrokRestViewWithFancyHtmlMixin
+from fancyhtmlview import HtmlContentType
