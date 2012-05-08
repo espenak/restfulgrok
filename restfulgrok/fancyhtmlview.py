f0fe236edb1041597e06f9274ebf6a96364f8f36
Espen A. Kristiansen <post@espenak.net>
2012-05-08 18:18:37 +0200

    Made some improvements to the fancyhtmlview.

--- restfulgrok/fancyhtmlview.py
+++ restfulgrok/fancyhtmlview.py
@@ -22,6 +22,22 @@ Read more on the <a href="http://yaml.org/">YAML website</a>.
 class GrokRestViewWithFancyHtmlMixin(GrokRestViewMixin):
     """
 
+    .. attribute:: template_debug
+
+        If ``True``, reload template on each request. If ``False``, cache the
+        template data in the class after first read.
+
+    .. attribute:: template_path
+
+        The ``pkg_resources.resource_string`` args for the template.
+        Defaults to::
+
+            ('restfulgrok', 'fancyhtmltemplate.jinja.html')
+
+    .. attribute:: html_pagetitle
+
+        Variable forwarded to the template as ``pagetitle``.
+        Defaults to: ``"REST API``.
     """
     default_content_type = 'text/html'
 
@@ -34,14 +50,28 @@ class GrokRestViewWithFancyHtmlMixin(GrokRestViewMixin):
     decoders = GrokRestViewMixin.decoders.copy()
     decoders['text/html'] = 'decode_null'
 
-    template_file = resource_string(__name__, 'fancyhtmltemplate.jinja.html')
+    template_debug = True
+    template_path = (__name__, 'fancyhtmltemplate.jinja.html')
     css = resource_string(__name__, 'bootstrap.min.css')
     html_pagetitle = 'REST API'
 
+    @classmethod
+    def get_templatedata(cls):
+        """
+        Load the template from :attr:`template_path`. If :attr:`template_debug`
+        is ``False``, cache the results.
+        """
+        if not cls.template_debug and hasattr(cls, 'template_data'):
+            return cls.template_data
+        else:
+            template_data = resource_string(*cls.template_path)
+            if not cls.template_debug:
+                cls.template_data = template_data
+            return template_data
+
     def encode_html(self, pydata):
-        self.template_file = resource_string(__name__, 'fancyhtmltemplate.jinja.html') # Comment this in when editing the template to see results on page refresh
         jsondata = self.encode_json(pydata)
-        template = Template(self.template_file)
+        template = Template(self.__class__.get_templatedata())
         return template.render(jsondata=jsondata,
                                css=self.css,
                                listed_contenttypes=self.html_listed_contenttypes,
