34b61776fe5d54d053a2be499d6228bc88b7c028
Espen A. Kristiansen <post@espenak.net>
2012-05-08 14:54:49 +0200

    Improved example.

--- restfulgrok/example.py
+++ restfulgrok/example.py
@@ -20,3 +20,19 @@ class ExampleRestViewMixin(GrokRestViewMixin):
             # Respond with 201 status and the request_data
             # NOTE: If you just return normally, 200 response status is used
             return self.response_201_created(request_data)
+
+    def handle_put(self):
+        try:
+            # Decode request body as a dict (JSON or YAML)
+            request_data = self.get_requestdata_dict()
+        except ValueError, e:
+            # Did not get a dict
+            return self.response_400_bad_request({'error': str(e)})
+        else:
+            data = request_data.copy() # pretend we got this from a database
+            data['last-modified'] = 'now' # Update some data
+            # would save here if this was real...
+
+            # NOTE: If you just return normally, 200 response status is used
+            return {'Updated': 'yes',
+                    'result': data}
--- restfulgrok/example_tests.py
+++ restfulgrok/example_tests.py
@@ -7,21 +7,30 @@ from mock import MockContext
 
 
 class MockExampleRestMixin(ExampleRestViewMixin):
-    def __init__(self, parentnode, id, method='GET', body=''):
+    def __init__(self, method='GET', body=''):
         self.response = MockResponse()
         self.request = MockRequest(method, body=body)
-        self.context = MockContext(parentnode, id)
+        self.context = MockContext()
 
 class TestExampleRestMixin(TestCase):
     def test_get(self):
-        result = MockExampleRestMixin(None, '10', 'GET').handle()
+        result = MockExampleRestMixin('GET').handle()
         self.assertEquals(result, {'hello': 'world'})
 
     def test_post(self):
         import json
         data = {'a': 'test'}
-        result = MockExampleRestMixin(None, '10', 'POST', body=json.dumps(data)).handle()
+        result = MockExampleRestMixin('POST', body=json.dumps(data)).handle()
         self.assertEquals(result, {'a': 'test'})
+
+    def test_put(self):
+        import json
+        data = {'a': 'test'}
+        result = MockExampleRestMixin('PUT', body=json.dumps(data)).handle()
+        self.assertEquals(result,
+                          {'Updated': 'yes',
+                           'result': {'a': 'test',
+                                      'last-modified': 'now'}})
 
 
 if __name__ == '__main__':
